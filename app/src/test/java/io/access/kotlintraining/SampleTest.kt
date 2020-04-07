e678529c96ec234412beeb403931301b65b0dc6b
HoangNguyen679 <ndhoang96@gmail.com>
2020-04-07 13:54:11 +0900

    exercise 1

--- app/src/test/java/io/access/kotlintraining/SampleTest.kt
+++ app/src/test/java/io/access/kotlintraining/SampleTest.kt
@@ -16,7 +16,43 @@ class SampleTest {
 
     @Test
     fun testHello() {
-        // TODO: implement to call hello function
         println("halo")
+    }
+
+    private fun fizzBuzz(number:Int) : String =
+        when {
+            number % 15 == 0 -> "fizzbuzz"
+            number % 3 == 0  -> "fizz"
+            number % 5 == 0  -> "buzz"
+            else             -> "$number"
+        }
+
+    @Test
+    fun testExercise1() {
+        for(number in 1..1000) {
+            println(fizzBuzz(number))
+        }
+    }
+
+    private fun isLeapYear(y: Int) : Boolean = y % 400 == 0 || (y % 100 != 0 && y % 4 == 0)
+
+    @Test
+    fun testLeapYear() {
+        assertEquals(true, isLeapYear(1600))
+    }
+
+    private fun power(a: Int, n: Int): Long {
+        if (a < 0 || n < 0) {
+            println("inputs must be positive. return 0")
+            return 0
+        }
+        var res:Long = 1
+        for (i in 1..n) res *= a
+        return res
+    }
+
+    @Test
+    fun testPower() {
+        assertEquals(16, power(2, 4))
     }
 }
